fec092697484bec06c2cea476897b0223fbc0d08
Eduardo Trujillo <ed@chromabits.com>
2015-05-03 21:23:39 -0400

    Use better function for getting client IP address

--- app/pings/factory.go
+++ app/pings/factory.go
@@ -1,11 +1,11 @@
 package pings
 
 import (
-	"net"
 	"net/http"
 
 	"github.com/etcinit/central/app/entities"
 	"github.com/etcinit/central/app/v1/requests"
+	"github.com/etcinit/speedbump/ginbump"
 )
 
 // Factory provides factory functions for creating instances of Ping entities.
@@ -13,7 +13,7 @@ type Factory struct{}
 
 // MakeFromV1 create a new Ping entitie from a API v1 request.
 func (f *Factory) MakeFromV1(request *http.Request, input *requests.PingJSON) *entities.Ping {
-	ip, _, _ := net.SplitHostPort(request.RemoteAddr)
+	ip := ginbump.GetRequesterAddress(request)
 
 	return &entities.Ping{
 		InstanceID: input.InstanceID,
