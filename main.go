67ccad05b020715298dee511adc3d4fa9b7479eb
Eduardo Trujillo <ed@chromabits.com>
2015-05-03 21:04:08 -0400

    Push version to 0.0.2

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func main() {
 	app.Usage = "Central Configuration Service"
 
 	// Set version and authorship info
-	app.Version = "0.0.1"
+	app.Version = "0.0.2"
 	app.Author = "Eduardo Trujillo <ed@chromabits.com>"
 
 	// Setup the default action. This action will be triggered when no
